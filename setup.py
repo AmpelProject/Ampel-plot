ad3b3100140927a213419a89bd65905c32bee27a
vbrinnel <valery.brinnel@gmail.com>
2021-02-23 00:22:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-plots/setup.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 23.02.2021
+# Last Modified Date: 23.02.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from setuptools import setup, find_namespace_packages # type: ignore
+
+setup(
+	name='ampel-plots',
+	version='0.7.2',
+	packages=find_namespace_packages(),
+	package_data = {
+		'': ['py.typed'],
+		'conf': [
+			'*.json', '**/*.json', '**/**/*.json',
+			'*.yaml', '**/*.yaml', '**/**/*.yaml',
+			'*.yml', '**/*.yml', '**/**/*.yml'
+		]
+	},
+	install_requires = [
+		"matplotlib",
+		"svgutils",
+		"cairosvg"
+	]
+)
