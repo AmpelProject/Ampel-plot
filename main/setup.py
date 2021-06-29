95a12017b1585d6651cba0145806f24483a076cf
vbrinnel <valery.brinnel@gmail.com>
2021-06-29 17:46:14 +0200

    New structure with 2 pkgs: ampel-plots & ampel-plots-cli

--- main/setup.py
+++ main/setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-plots/setup.py
+# File              : Ampel-plots/main/setup.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 23.02.2021
-# Last Modified Date: 23.02.2021
+# Last Modified Date: 29.06.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from setuptools import setup, find_namespace_packages # type: ignore
 
 setup(
 	name='ampel-plots',
-	version='0.7.2',
+	version='0.8.0',
 	packages=find_namespace_packages(),
 	package_data = {
 		'': ['py.typed'],
@@ -21,9 +21,5 @@ setup(
 			'*.yml', '**/*.yml', '**/**/*.yml'
 		]
 	},
-	install_requires = [
-		"matplotlib",
-		"svgutils==0.3.0",
-		"cairosvg"
-	]
+	install_requires = ["matplotlib", "svgutils==0.3.0", "cairosvg"]
 )
