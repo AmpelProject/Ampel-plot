bed9a3b7496c0364f0f5aed3909438682bc77b2e
vbrinnel <valery.brinnel@gmail.com>
2021-09-17 11:52:01 +0200

    SVGLoader: better plot and tag matching

--- main/ampel/plot/SVGLoader.py
+++ main/ampel/plot/SVGLoader.py
@@ -4,22 +4,29 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.06.2019
-# Last Modified Date: 15.09.2021
+# Last Modified Date: 17.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Optional, Sequence, Union, Dict, TYPE_CHECKING
 from collections import defaultdict
+from string import digits
 
-from ampel.types import StockId, UnitId, Tag, List, Any
+from ampel.types import StockId, UnitId, Tag, List
 from ampel.core.AmpelDB import AmpelDB
 from ampel.content.SVGRecord import SVGRecord
 from ampel.log.AmpelLogger import AmpelLogger
 from ampel.plot.SVGQuery import SVGQuery
 from ampel.plot.T2SVGQuery import T2SVGQuery
 from ampel.plot.SVGCollection import SVGCollection
+from ampel.util.mappings import walk_and_process_dict
+from ampel.model.operator.AnyOf import AnyOf
+from ampel.model.operator.AllOf import AllOf
+from ampel.model.operator.OneOf import OneOf
 
 if TYPE_CHECKING:
 	from ampel.plot.SVGBrowser import SVGBrowser
+
+remove_digits = str.maketrans('', '', digits)
 
 
 class SVGLoader:
@@ -56,13 +63,19 @@ class SVGLoader:
 		db: AmpelDB,
 		queries: Optional[List[SVGQuery]] = None,
 		logger: Optional[AmpelLogger] = None,
+		last_body: bool = False,
+		enforce_base_path: bool = False,
 		limit: int = 0
 	) -> None:
 		self._db = db
 		self.logger = logger
 		self.limit = limit
+		self.last_body = last_body
+		self.enforce_base_path = enforce_base_path
 		self._queries: List[SVGQuery] = []
 		self._plots: Dict[StockId, SVGCollection] = defaultdict(SVGCollection)
+		self._debug = self.logger and self.logger.verbose > 1
+		
 		if queries:
 			for q in queries:
 				self.add_query(q)
@@ -84,7 +97,7 @@ class SVGLoader:
 
 		for q in self._queries:
 
-			if self.logger and self.logger.verbose > 1:
+			if self._debug:
 				self.logger.debug(f"Running {q.col} query: {q._query}")
 
 			if self.limit:
@@ -92,26 +105,53 @@ class SVGLoader:
 			else:
 				res = self._db.get_collection(q.col).find(q._query)
 
-			if self.logger and self.logger.verbose > 1:
+			if self._debug:
 				self.logger.debug(f"{res.count()} documents matched")
+	
+			for el in res:
 
-			if q.path == "plot": # root
-				for el in res:
-					i += 1
-					if self.logger and self.logger.verbose > 1:
-						self.logger.debug(f"Parsing {el['_id']}")
-					self._load_plots(q, el['stock'], el['plot'])
-			elif q.path == "body.data.plot": # convention
-				for el in res:
-					i += 1
-					if self.logger and self.logger.verbose > 1:
-						self.logger.debug(f"Parsing {el['_id']}")
-					if 'body' in el: # t1 docs do not necessarily have body
-						if isinstance(el['body'], list):
-							for ell in el['body']:
-								self._load_body_el(q, el['stock'], ell)
-						if isinstance(el['body'], dict):
-							self._load_body_el(q, el['stock'], el['body'])
+				i += 1
+				if self._debug:
+					self.logger.debug(f"Parsing {el['_id']}")
+
+				if not el.get('body'):
+					if self._debug:
+						self.logger.debug(" Skipping doc: empty body")
+					continue
+
+				stock = el.get('stock', 0)
+
+				if isinstance(el['body'], list):
+					if self.last_body:
+						walk_and_process_dict(
+							arg = el['body'][-1],
+							callback = self._gather_plots_callback,
+							match = ['plot'],
+							q = q,
+							stock = stock
+						)
+					else:
+						for ell in el['body']:
+							walk_and_process_dict(
+								arg = ell,
+								callback = self._gather_plots_callback,
+								match = ['plot'],
+								q = q,
+								stock = stock
+							)
+				elif isinstance(el['body'], dict):
+					walk_and_process_dict(
+						arg = el['body'],
+						callback = self._gather_plots_callback,
+						match = ['plot'],
+						q = q,
+						stock = stock
+					)
+				else:
+					if self._debug:
+						self.logger.debug(f" Skipping doc: unrecognized body type ({type(el['body'])}")
+
+			#	self._load_plots(q, stock, get_by_path(el['body'], rel_path) if nested else el['body'][rel_path])
 
 			if self.limit and self.limit > i:
 				break
@@ -119,44 +159,52 @@ class SVGLoader:
 		return self
 
 
-	def _load_body_el(self, q: SVGQuery, stock: StockId, arg: Any, k: str = 'data') -> None:
+	def _gather_plots_callback(self, path, k, d, **kwargs) -> None:
 
-		if k not in arg:
+		if self.enforce_base_path and kwargs['q'].path:
+			# quick n dirty (to be improved)
+			# will yield troubles if path keys contain numbers
+			# will yield troubles with double digit sequence ('body.data.0.21.tra')
+			# but those can't be used for matching anyway
+			sequenceless_path = "body." + path.translate(remove_digits).replace("..", "")
+			if sequenceless_path not in kwargs['q'].path:
+				if self._debug:
+					self.logger.debug(
+						f" Ignoring plot(s) with path body.{path}.plot "
+						f" (Path {sequenceless_path[:-1]} is outside base path {kwargs['q'].path})"
+					)
+				return
+				
+		if self._debug:
+			if path:
+				self.logger.debug(f" Loading plot(s) with path body.{path}.plot")
+			else:
+				self.logger.debug(" Loading plot(s) with path body.plot")
+
+		self._load_plots(kwargs['q'], kwargs['stock'], d[k])
+
+
+	def _load_plots(self, query: SVGQuery, stock: StockId, plots: Sequence[SVGRecord]) -> None:
+
+		if not plots:
 			return
 
-		if isinstance(stock, list):
-			stock = tuple(stock)
+		for i, p in enumerate(plots):
 
-		if isinstance(arg[k], dict):
-			if 'plot' not in arg[k]:
-				return
-			self._load_plots(q, stock, arg[k]['plot'])
+			if self._debug:
+				self.logger.debug(f"Loading plot with index {i}")
 
-		elif isinstance(arg[k], list):
-			for el in arg[k]:
-				if isinstance(el, dict) and "plot" in el:
-					self._load_plots(q, stock, el['plot'])
-
-
-	def _load_plots(self,
-		query: SVGQuery, stock: StockId, plots: Sequence[SVGRecord]
-	) -> None:
-
-		if self.logger and self.logger.verbose > 1:
-			self.logger.debug("Loading plots")
-
-		for p in plots:
-
-			#if query.tag:
-			#	if isinstance(query.tag, (list, tuple)):
-			#		if isinstance(p['tag'], list) and not all(x in p['tag'] for x in query.tag):
-			#			continue
-			#		elif isinstance(p['tag'], (int, str)) and p['tag'] not in query.tag:
-			#			continue
-			##	else:
-			#		if isinstance(p['tag'], list) and query.tag not in p['tag']:
-			#			continue
-			#		elif isinstance(p['tag'], (int, str)) and query.tag != p['tag']:
-			#			continue
+			if query.plot_tag:
+				if 'with' in query.plot_tag:
+					wqt = query.plot_tag['with']
+					if (
+						(isinstance(wqt, AllOf) and not all(x in p['tag'] for x in wqt.all_of)) or
+						(isinstance(wqt, AnyOf) and not [x in p['tag'] for x in wqt.any_of]) or
+						(isinstance(wqt, OneOf) and not wqt.any_of == [p['tag']]) or
+						(isinstance(wqt, (int, str)) and wqt not in p['tag'])
+					):
+						if self.logger and self.logger.verbose > 1:
+							self.logger.debug("Excluding plot (tag matching failed)")
+						continue
 
 			self._plots[stock].add_raw_db_dict(p)
