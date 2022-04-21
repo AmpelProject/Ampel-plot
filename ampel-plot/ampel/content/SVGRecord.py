59a533b50ee0cac2e637d2b953dae9aafec587f2
valery brinnel <valery.brinnel@gmail.com>
2022-04-21 17:36:03 +0200

    SVGRecord: minor typing update

--- ampel-plot/ampel/content/SVGRecord.py
+++ ampel-plot/ampel/content/SVGRecord.py
@@ -4,20 +4,19 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                13.02.2021
-# Last Modified Date:  22.02.2021
+# Last Modified Date:  20.04.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from typing import TypedDict
+from typing_extensions import NotRequired
 from collections.abc import Sequence
 from ampel.types import Tag
 
 
 class SVGRecord(TypedDict, total=False):
-	"""
-	Dict crafted by :class:`~ampel.plot.utils.mplfig_to_svg_dict`
-	"""
+
 	name: str
-	tag: Tag | Sequence[Tag]
 	title: str
+	tag: Tag | Sequence[Tag]
 	svg: bytes | str # bytes means compressed svg
-	svg_str: str
+	svg_str: NotRequired[str]
