df650e7347d0647001e3fa511f5a7bccad87eab4
valery brinnel <valery.brinnel@gmail.com>
2022-05-13 16:25:09 +0200

    move AmpelPlotAdapter package

--- ampel-plot/ampel/core/adapter/AmpelPlotAdapter.py
+++ ampel-plot/ampel/core/adapter/AmpelPlotAdapter.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File:                Ampel-plot/ampel-plot/ampel/t3/adapter/AmpelPlotAdapter.py
+# File:                Ampel-plot/ampel-plot/ampel/core/adapter/AmpelPlotAdapter.py
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                20.04.2022
-# Last Modified Date:  27.04.2022
+# Last Modified Date:  13.05.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from bson import ObjectId # type: ignore[import]
 from ampel.util.recursion import walk_and_process_dict
-from ampel.abstract.AbsT3UnitResultAdapter import AbsT3UnitResultAdapter
+from ampel.abstract.AbsUnitResultAdapter import AbsUnitResultAdapter
 from ampel.struct.UnitResult import UnitResult
 from ampel.view.T3Store import T3Store
 
 
-class AmpelPlotAdapter(AbsT3UnitResultAdapter):
+class AmpelPlotAdapter(AbsUnitResultAdapter):
 	"""
 	Provides logic for handling plots embedded in UnitResult.
 	These will be saved into dedicated collection
 	"""
 
-	def handle(self, ur: UnitResult, t3s: T3Store,) -> UnitResult:
+	def handle(self, ur: UnitResult) -> UnitResult:
 
 		if ur.body is None or not isinstance(ur.body, (dict, list)):
 			raise ValueError()
