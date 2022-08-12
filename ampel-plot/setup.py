a65baa240778d4f4125c26d0d0476595e3ce470a
Jakob van Santen <jvansanten@gmail.com>
2022-08-12 13:23:02 +0200

    setup: include description in wheel

--- ampel-plot/setup.py
+++ ampel-plot/setup.py
@@ -8,10 +8,13 @@
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from setuptools import setup, find_namespace_packages # type: ignore
+from pathlib import Path
 
 setup(
 	name='ampel-plot',
 	version='0.8.3',
+	long_description=(Path(__file__).parent / "README.md").read_text(),
+	long_description_content_type="text/markdown",
 	packages=find_namespace_packages(),
 	package_data = {
 		'': ['py.typed'],
