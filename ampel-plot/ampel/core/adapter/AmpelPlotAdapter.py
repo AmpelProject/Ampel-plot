94363c9e4709edb4ec9e2cfbc4cef1737ded3b9f
valery brinnel <valery.brinnel@gmail.com>
2022-08-28 21:28:27 +0200

    AmpelPlotAdapter: include run in detached plots

--- ampel-plot/ampel/core/adapter/AmpelPlotAdapter.py
+++ ampel-plot/ampel/core/adapter/AmpelPlotAdapter.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                20.04.2022
-# Last Modified Date:  17.05.2022
+# Last Modified Date:  28.08.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from bson import ObjectId # type: ignore[import]
@@ -26,7 +26,7 @@ class AmpelPlotAdapter(AbsUnitResultAdapter):
 
 		walk_and_process_dict(
 			arg = ur.body,
-			callback = _insert_plots,
+			callback = self.insert_plots,
 			match = ['plot'],
 			col = self.context.db.get_collection('plot')
 		)
@@ -34,23 +34,26 @@ class AmpelPlotAdapter(AbsUnitResultAdapter):
 		return ur
 
 
-def _insert_plots(path, k, d, **kwargs) -> None:
-	""" Used by walk_and_process_dict(...) """
+	def insert_plots(self, path, k, d, **kwargs) -> None:
+		""" Used by walk_and_process_dict(...) """
 
-	if isinstance(d[k], dict):
-		if d[k].get('detached'):
-			d[k]['svg'] = kwargs['col'].insert_one(d[k]).inserted_id
-			del d[k]['detached']
+		if isinstance(d[k], dict):
+			if d[k].get('detached'):
+				d[k]['run'] = self.run_id
+				d[k]['svg'] = kwargs['col'].insert_one(d[k]).inserted_id
+				del d[k]['run']
+				del d[k]['detached']
 
-	elif isinstance(d[k], list) and d[k]:
-		insert = []
-		for i in range(len(d[k])):
-			d2 = d[k][i]
-			if d2.get('detached'):
-				del d2['detached']
-				d3 = d2.copy()
-				d3['_id'] = ObjectId()
-				d2['svg'] = d3['_id']
-				insert.append(d3)
+		elif isinstance(d[k], list) and d[k]:
+			insert = []
+			for i in range(len(d[k])):
+				d2 = d[k][i]
+				if d2.get('detached'):
+					del d2['detached']
+					d3 = d2.copy()
+					d3['_id'] = ObjectId()
+					d3['run'] = self.run_id
+					d2['svg'] = d3['_id']
+					insert.append(d3)
 
-		kwargs['col'].insert_many(insert)
+			kwargs['col'].insert_many(insert)
