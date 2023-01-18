259cc85c88aa4f28f954e31a1435ea5e2aae1a6a
valery brinnel <valery.brinnel@gmail.com>
2023-01-18 22:35:22 +0100

    Bump version: v0.8.3-2

--- ampel-plot/setup.py
+++ ampel-plot/setup.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                23.02.2021
-# Last Modified Date:  11.01.2023
+# Last Modified Date:  18.01.2023
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from setuptools import setup, find_namespace_packages # type: ignore
@@ -12,7 +12,7 @@ from pathlib import Path
 
 setup(
 	name='ampel-plot',
-	version='0.8.3-1',
+	version='0.8.3-2',
 	long_description=(Path(__file__).parent / "README.md").read_text(),
 	long_description_content_type="text/markdown",
 	packages=find_namespace_packages(),
